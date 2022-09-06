a5dca40819a0969a5d901cb22f2f087df3f0a5af
petermonky <wjdgustjr0554@gmail.com>
2022-09-06 17:57:17 +0800

    feat: add ormFindUser method in user-orm layer

--- user-service/model/user-orm.js
+++ user-service/model/user-orm.js
@@ -1,14 +1,23 @@
-import { createUser } from './repository.js';
+import { createUser, findUser } from "./repository.js";
 
 //need to separate orm functions from repository to decouple business logic from persistence
 export async function ormCreateUser(username, password) {
     try {
-        const newUser = await createUser({username, password});
+        const newUser = await createUser({ username, password });
         newUser.save();
         return true;
     } catch (err) {
-        console.log('ERROR: Could not create new user');
+        console.log("ERROR: Could not create new user");
         return { err };
     }
 }
 
+export async function ormFindUser(username) {
+    try {
+        const user = await findUser({ username });
+        return user;
+    } catch (err) {
+        console.log("ERROR: Could not find existing user");
+        return { err };
+    }
+}
