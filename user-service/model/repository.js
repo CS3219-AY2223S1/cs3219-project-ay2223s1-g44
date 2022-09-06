fc9148aeb80a7df6f190b7c2685d13144e407b7b
petermonky <wjdgustjr0554@gmail.com>
2022-09-06 17:57:03 +0800

    feat: add findUser method in repository layer

--- user-service/model/repository.js
+++ user-service/model/repository.js
@@ -1,30 +1,20 @@
-import UserModel from './user-model.js';
-import 'dotenv/config'
+import "dotenv/config";
+import UserModel from "./user-model.js";
 
 //Set up mongoose connection
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
 
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-export async function createUser(params) { 
-  return new UserModel(params)
+export async function createUser(params) {
+    return new UserModel(params);
 }
 
-export async function doesUserExist(newUser) { 
-  const user = UserModel.find({username: newUser.username});
-  return user;
+export async function findUser(params) {
+    return UserModel.findOne({ username: params.username });
 }
-
-export async function correctUserandPwd(newUser) { 
-  const user = UserModel.find({username: newUser.username, password: newUser.password});
-  return user;
-}
-
-
-
-
