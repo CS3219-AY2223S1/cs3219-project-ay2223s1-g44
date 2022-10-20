9433585c2f04a0eaf065d1ca275a9324f068d8f1
petermonky <wjdgustjr0554@gmail.com>
2022-10-20 19:31:09 +0800

    [ts-migrate][collaboration-service] Run TS Migrate
    Co-authored-by: ts-migrate <>

--- collaboration-service/index.ts
+++ collaboration-service/index.ts
@@ -22,60 +22,49 @@ const matchIdDocMap = new Map();
 
 io.on('connection', (socket) => {
     // When connected, put both users in the same room
-    let matchIdHolder, userHolder;
-    console.log(socket.id + " joined room")
-
+    let matchIdHolder: any, userHolder: any;
+    console.log(socket.id + " joined room");
     socket.on('joinRoom', (obj) => {
         const { matchId, user } = obj;
         matchIdHolder = matchId;
         userHolder = user;
-
         socket.join(matchId);
         if (!matchIdDocMap.has(matchId)) {
             matchIdDocMap.set(matchId, Automerge.change(Automerge.init(), (doc) => {
-                doc.text = new Automerge.Text('');
-                return doc.text;
+                (doc as any).text = new Automerge.Text('');
+                return (doc as any).text;
             }));
         }
-
         const changes = Automerge.getAllChanges(matchIdDocMap.get(matchId));
         socket.emit('joinRoomSuccess', { changes });
     });
-
     socket.on('updateCode', (changes) => {
         const oldDoc = matchIdDocMap.get(matchIdHolder);
-        const [doc] = Automerge.applyChanges(
-            Automerge.clone(oldDoc),
-            changes
-        );
+        const [doc] = Automerge.applyChanges(Automerge.clone(oldDoc), changes);
         matchIdDocMap.set(matchIdHolder, doc);
         socket.to(matchIdHolder).emit('updateCodeSuccess', changes);
-    })
-
+    });
     // Track the code for both side, so when every someone edits, the whole code is sent to
     // the other party.
     socket.on('codeEditor', (code) => {
-        console.log(code)
+        console.log(code);
         socket.to(matchIdHolder).emit('codeEditor', code);
-    })
-
+    });
     // Track set language for both parties
     socket.on('setLanguage', (lang) => {
-        socket.to(matchIdHolder).emit('setLanguage', lang)
-    })
-
+        socket.to(matchIdHolder).emit('setLanguage', lang);
+    });
     //Tracker for chat bot
     socket.on('chatBox', (message) => {
-        console.log(message)
-        socket.to(matchIdHolder).emit('chatBox', message)
-    })
-
+        console.log(message);
+        socket.to(matchIdHolder).emit('chatBox', message);
+    });
     socket.on('disconnect', (reason) => {
-        console.log(socket.id + reason)
-        const leaveRoomMessage = String(userHolder?.username) + " has left the room"
-        io.to(matchIdHolder).emit('chatBox', leaveRoomMessage)
-    })
-})
+        console.log(socket.id + reason);
+        const leaveRoomMessage = String(userHolder?.username) + " has left the room";
+        io.to(matchIdHolder).emit('chatBox', leaveRoomMessage);
+    });
+});
 
 // Http server listen
 server.listen(port, host, undefined, () => console.log(`Server running on port ${host} ${port}`));
