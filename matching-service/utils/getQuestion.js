945895b6307a9897cd71a60a4107ef919d81fae0
Zhou-Jiahao-1998 <jasonzhoujiahao@gmail.com>
2022-10-21 15:05:46 +0800

    fix: update getRandomQuestion

--- matching-service/utils/getQuestion.js
+++ matching-service/utils/getQuestion.js
@@ -4,7 +4,7 @@ const QUESTION_URL = 'http://localhost:5000/questions';
 
 export async function getRandomQuestion(difficulty) {
     try {
-        const question = (await axios.get(QUESTION_URL + `/${difficulty}`)).data;
+        const question = await axios.get(QUESTION_URL + `/${difficulty}`);
         const { data } = question;
         return data;
     } catch (err) {
@@ -14,7 +14,7 @@ export async function getRandomQuestion(difficulty) {
 
 export async function getQuestion(difficulty, id) {
     try {
-        const question = (await axios.get(QUESTION_URL + `/${difficulty}/${id}`));
+        const question = await axios.get(QUESTION_URL + `/${difficulty}/${id}`);
         const { data } = question;
         return data;
     }
