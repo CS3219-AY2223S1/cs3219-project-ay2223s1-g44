8f3f23a85911233aa0a69f7cf2542b99d4e0f527
Zhou-Jiahao-1998 <jasonzhoujiahao@gmail.com>
2022-10-21 10:26:02 +0800

    feat: add err handling for getQuestion

--- matching-service/utils/getQuestion.js
+++ matching-service/utils/getQuestion.js
@@ -3,11 +3,20 @@ import axios from "axios";
 const QUESTION_URL = 'http://localhost:5000/questions';
 
 export async function getRandomQuestion(difficulty) {
-    const question = (await axios.get(QUESTION_URL + `/${difficulty}`)).data;
-    return question
+    try {
+        const question = (await axios.get(QUESTION_URL + `/${difficulty}`)).data;
+        return question;
+    } catch (err) {
+        return err;
+    }
 }
 
 export async function getQuestion(difficulty, id) {
-    const question = (await axios.get(QUESTION_URL + `/${difficulty}/${id}`)).data;
-    return question
+    try {
+        const question = (await axios.get(QUESTION_URL + `/${difficulty}/${id}`)).data;
+        return question;
+    }
+    catch (err) {
+        return err;
+    }
 }
