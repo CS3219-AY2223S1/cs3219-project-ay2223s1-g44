177f23a6da3446685b12ed734b38ede9bf7fd807
petermonky <wjdgustjr0554@gmail.com>
2022-10-27 14:49:58 +0800

    feat: refactor and make landing page responsive

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -12,7 +12,6 @@ import {
   Link,
   Popover,
   PopoverTrigger,
-  PopoverContent,
   useColorModeValue,
   useBreakpointValue,
   useDisclosure,
@@ -21,50 +20,11 @@ import {
   HamburgerIcon,
   CloseIcon,
   ChevronDownIcon,
-  ChevronRightIcon,
 } from '@chakra-ui/icons';
 
 import { authContext } from '../../hooks/useAuth';
 
 import { NavItemProps, ROUTES } from '../../configs';
-
-function DesktopSubNav({ label, href, subLabel }: NavItemProps) {
-  return (
-    <Link
-      as={RouterLink}
-      to={href ?? '#'}
-      role="group"
-      display="block"
-      p={2}
-      rounded="md"
-      _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}
-    >
-      <Stack direction="row" align="center">
-        <Box>
-          <Text
-            transition="all .3s ease"
-            _groupHover={{ color: 'pink.400' }}
-            fontWeight={500}
-          >
-            {label}
-          </Text>
-          <Text fontSize="sm">{subLabel}</Text>
-        </Box>
-        <Flex
-          transition="all .3s ease"
-          transform="translateX(-10px)"
-          opacity={0}
-          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
-          justify="flex-end"
-          align="center"
-          flex={1}
-        >
-          <Icon color="pink.400" w={5} h={5} as={ChevronRightIcon} />
-        </Flex>
-      </Stack>
-    </Link>
-  );
-}
 
 type DesktopNavProps = {
   navItems: Array<NavItemProps>
@@ -73,7 +33,6 @@ type DesktopNavProps = {
 function DesktopNav({ navItems }: DesktopNavProps) {
   const linkColor = useColorModeValue('brand-gray.2', 'gray.200');
   const linkHoverColor = useColorModeValue('brand-gray.3', 'white');
-  const popoverContentBgColor = useColorModeValue('white', 'gray.800');
 
   return (
     <Stack direction="row" spacing={4}>
@@ -96,23 +55,6 @@ function DesktopNav({ navItems }: DesktopNavProps) {
                 {navItem.label}
               </Link>
             </PopoverTrigger>
-
-            {navItem.children && (
-            <PopoverContent
-              border={0}
-              boxShadow="xl"
-              bg={popoverContentBgColor}
-              p={4}
-              rounded="xl"
-              minW="sm"
-            >
-              <Stack>
-                {navItem.children.map((child) => (
-                  <DesktopSubNav key={child.label} {...child} />
-                ))}
-              </Stack>
-            </PopoverContent>
-            )}
           </Popover>
         </Box>
       ))}
@@ -214,8 +156,9 @@ export default function NavBar() {
         position="relative"
         zIndex={1}
       >
+        {/* mobile menu */}
         <Flex
-          flex={{ base: 1, md: 'auto' }}
+          grow={{ base: 1, md: 'auto' }}
           ml={{ base: -2 }}
           display={{ base: 'flex', md: 'none' }}
         >
@@ -228,7 +171,7 @@ export default function NavBar() {
             aria-label="Toggle Navigation"
           />
         </Flex>
-        <Flex flex={{ base: 1 }}>
+        <Flex grow={{ base: 1 }} justify={{ base: 'center', md: 'flex-start' }}>
           <Text
             textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
             as={RouterLink}
@@ -244,50 +187,51 @@ export default function NavBar() {
             <DesktopNav navItems={navItems} />
           </Flex>
         </Flex>
-
-        {!isAuthed
-        && (
-        <Stack
-          flex={{ base: 1, md: 0 }}
+        <Flex
+          grow={{ base: 1, md: 0 }}
           justify="flex-end"
           direction="row"
-          spacing={6}
+          gap={6}
         >
-          <Button
-            as={RouterLink}
-            to="login"
-            fontSize={12}
-            fontWeight={500}
-            color="brand-gray.2"
-            variant="link"
-            _active={
-              { color: 'brand-gray.3' }
-            }
-          >
-            Log In
-          </Button>
-          <Button
-            as={RouterLink}
-            to="register"
-            display={{ base: 'none', md: 'inline-flex' }}
-            fontSize={12}
-            fontWeight={500}
-            color="brand-white"
-            bg="brand-blue.1"
-            variant="button"
-            borderRadius={8}
-            height="40px"
-            _hover={
-              { bg: 'brand-blue.2' }
-            }
-            _active={
-              { bg: 'brand-blue.3' }
-            }
-          >
-            Sign Up
-          </Button>
-        </Stack>
-        )}
+          {!isAuthed
+            && (
+            <>
+              <Button
+                as={RouterLink}
+                to="login"
+                fontSize={12}
+                fontWeight={500}
+                color="brand-gray.2"
+                variant="link"
+                _active={
+                  { color: 'brand-gray.3' }
+                }
+              >
+                Log In
+              </Button>
+              <Button
+                as={RouterLink}
+                to="register"
+                display={{ base: 'none', md: 'inline-flex' }}
+                fontSize={12}
+                fontWeight={500}
+                color="brand-white"
+                bg="brand-blue.1"
+                variant="button"
+                borderRadius={8}
+                height="40px"
+                _hover={
+                  { bg: 'brand-blue.2' }
+                }
+                _active={
+                  { bg: 'brand-blue.3' }
+                }
+              >
+                Sign Up
+              </Button>
+            </>
+            )}
+        </Flex>
       </Flex>
 
       <Collapse in={isOpen} animateOpacity>
