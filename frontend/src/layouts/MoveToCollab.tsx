089e687585f8c2ef289c5cd604aa2e0dfb568698
Zhou-Jiahao-1998 <jasonzhoujiahao@gmail.com>
2022-10-30 00:09:38 +0800

    feat: add button to redirect when has active match

---
+++ frontend/src/layouts/MoveToCollab.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from '@chakra-ui/react';
+
+export default function MoveToCollab() {
+  return (
+    <>
+      You have an active matching!
+      <Link href="collab-space">Go to Collaboration Room</Link>
+    </>
+  );
+}
--- frontend/src/layouts/ProtectedLayout.tsx
+++ frontend/src/layouts/ProtectedLayout.tsx
@@ -1,20 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 import { authContext } from '../hooks/useAuth';
 import { useMatchDetail } from '../hooks/useMatch';
+import MoveToCollab from './MoveToCollab';
 
 function ProtectedLayout() {
   const auth = useContext(authContext);
   const { match, question, matchLoading } = useMatchDetail(auth.user.username);
   const { isLoading, isAuthed } = auth;
+  const [hasActive, setActive] = useState(false);
+
+  useEffect(() => {
+    if (match._id !== '' && window.location.pathname !== '/collab-space') {
+      setActive(true);
+    }
+  }, [match]);
 
   if (isLoading || matchLoading) {
     return null;
   }
 
-  if (match._id !== '' && window.location.pathname !== '/collab-space') {
-    return <Navigate to="/collab-space" replace />;
+  if (hasActive) {
+    return <MoveToCollab />;
   }
 
   return !isAuthed ? <Navigate to="/login" replace /> : <Outlet />;
